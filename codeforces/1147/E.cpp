74b151c2f3b9289bb2edc46811073bf55c8180b5
JustBeYou <mihailferaru2000@gmail.com>
2019-05-06 22:47:08 +0300

    update

---
+++ codeforces/1147/E.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long ull;
+typedef signed long long ll;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+const int nmax = 1e5;
+
+void prep() {
+#ifndef LOCAL
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+#endif
+}
+
+int main() {
+    prep();
+
+    return 0;
+}
